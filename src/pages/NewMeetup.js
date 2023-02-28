74ec448e2254ee9cbec3f20d8869951456114667
Dina Rosita <dina@koudys.com>
2023-02-28 17:18:18 -0500

    Learn up to firebase - React Refresh 43

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,10 +2,11 @@ import React from 'react'
 import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 export default function NewMeetupPage() {
+    function addMeetupHandler(meetupData) {}
   return (
     <section>
       <h1>New Meetup Page</h1>
-      <NewMeetupForm><h3>Howsy</h3><p>horsy</p></NewMeetupForm>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   )
 }
